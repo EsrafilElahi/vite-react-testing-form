92113ada358bd324c73462470311eb8ed13a5e78
esrafilelahi <essinho1121@gmail.com>
2023-07-29 22:06:37 +0330

    add form

---
+++ src/component/Form.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import '../styles/styles.css'
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validationErrors = validateForm(formData);
+    if (Object.keys(validationErrors).length === 0) {
+      // Form data is valid, you can perform your desired action here (e.g., submit to the server)
+      console.log("Form submitted:", formData);
+    } else {
+      setErrors(validationErrors);
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!data.email) {
+      errors.email = "Email is required";
+    } else if (!emailRegex.test(data.email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!data.password) {
+      errors.password = "Password is required";
+    } else if (data.password.length < 6) {
+      errors.password = "Password must be at least 6 characters long";
+    }
+
+    if (!data.confirmPassword) {
+      errors.confirmPassword = "Please confirm your password";
+    } else if (data.password !== data.confirmPassword) {
+      errors.confirmPassword = "Passwords don't match";
+    }
+
+    return errors;
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className={errors.email ? "error" : ""}
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          className={errors.password ? "error" : ""}
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          className={errors.confirmPassword ? "error" : ""}
+        />
+        {errors.confirmPassword && (
+          <p className="error-text">{errors.confirmPassword}</p>
+        )}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
